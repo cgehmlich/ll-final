db4ef6967e3ce6a60cc49549f735dc79f08333df
Jayden Shaw <jayden.shaw@gmail.com>
2018-12-14 05:20:51 +0000

    CSS testing. User Nav Comp added. Room Comp added. All comps added to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import Heading from './Header.jsx';
-import Player from './player.jsx';
+import Player from './Player.jsx';
 import LoginPage from './Signin.jsx';
 import RegisterPage from './Signup.jsx';
+import User from './User.jsx';
+import Room from './RoomComp.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>  
           <Heading/>
+          <User/>
+          <Player/>
           <LoginPage/>
           <RegisterPage/>
+          <Room/>
       </div>
     );
   }
---
+++ src/RoomComp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Media } from 'reactstrap';
+
+const Room = () => {
+  return (
+    <Media class="room">
+      <Media body>
+        <Media heading>
+          Media heading
+        </Media>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </Media>
+    </Media>
+  );
+};
+
+export default Room;
