e4bc47860bed7a52ea56d2341ef5b665676fde26
cgehmlich <cgehmlich@gmail.com>
2018-12-17 21:34:08 +0000

    seeding

--- db/migrations/20181213213016_create_users.js
+++ db/migrations/20181213213016_create_users.js
@@ -2,6 +2,8 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', function (table) {
       table.increments();
       table.string('name');
+      table.string('email');
+      table.string('password')
       table.specificType('followers', 'text ARRAY');
       table.specificType('following', 'text ARRAY');
     });
